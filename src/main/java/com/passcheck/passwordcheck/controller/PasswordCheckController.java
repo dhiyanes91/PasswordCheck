61a81d264a2291fe3681fa934e9b48abb1eede01
Dhiyaneswaran <dhiyanes91@gmail.com>
2021-04-29 20:28:40 +0530

    Adding Respective Controller and Service Methods

---
+++ src/main/java/com/passcheck/passwordcheck/controller/PasswordCheckController.java
@@ -0,0 +1,20 @@
+package com.passcheck.passwordcheck.controller;
+
+import com.passcheck.passwordcheck.service.PasswordCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PasswordCheckController {
+
+    @Autowired
+    PasswordCheckService passwordCheckService;
+
+    @GetMapping(value="/verifyPassword")
+    public @ResponseBody Boolean doPasswordVerification (@RequestParam String password){
+        return passwordCheckService.doPasswordVerification(password);
+    }
+}
---
+++ src/main/java/com/passcheck/passwordcheck/service/PasswordCheckService.java
@@ -0,0 +1,11 @@
+package com.passcheck.passwordcheck.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordCheckService {
+
+    public Boolean doPasswordVerification(String password) {
+        return false;
+    }
+}
