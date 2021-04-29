540d28a883d43eec0d70c409de0451dee5520772
Dhiyaneswaran <dhiyanes91@gmail.com>
2021-04-29 19:47:57 +0530

    Project initialization

---
+++ src/test/java/com/passcheck/passwordcheck/PasswordCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passcheck.passwordcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
