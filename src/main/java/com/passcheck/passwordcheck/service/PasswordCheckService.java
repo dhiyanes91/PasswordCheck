bf0486fbaa50f6967ed862c9faf247911fa009c0
Dhiyaneswaran <dhiyanes91@gmail.com>
2021-04-29 21:20:40 +0530

    Point 1 TDD completion

--- src/main/java/com/passcheck/passwordcheck/service/PasswordCheckService.java
+++ src/main/java/com/passcheck/passwordcheck/service/PasswordCheckService.java
@@ -6,6 +6,20 @@ import org.springframework.stereotype.Service;
 public class PasswordCheckService {
 
     public Boolean doPasswordVerification(String password) {
-        return false;
+        Boolean isOkPassword = false;
+        if(password != null) {
+            if(! password.isEmpty()) {
+                if(password.length() > 8) {
+                    if(password.matches(".*[A-Z].*")){
+                        if(password.matches(".*[a-z].*")){
+                            if(password.matches(".*[0-9].*")){
+                                isOkPassword = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return isOkPassword;
     }
 }
